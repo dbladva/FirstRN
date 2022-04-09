0275f4763f8ed4b3c9da21ef9c9c70da2a348cde
dbladva <ladvadharmesh03@gmail.com>
2022-04-09 17:44:17 +0530

    ds
    sd

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import First from './src/containers/First'
-import Demo from './src/containers/Demo'
-import MedicineData from './src/containers/MedicineData'
+import {View, Text} from 'react-native';
+import React from 'react';
+import First from './src/containers/First';
+import Demo from './src/containers/Demo';
+import MedicineData from './src/containers/MedicineData';
 
 export default function App() {
   return (
     <View>
-      
-      <MedicineData />
       <First />
       <Demo />
+      <MedicineData />
     </View>
-  )
+  );
 }
