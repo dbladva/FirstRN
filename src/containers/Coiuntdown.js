1674aab56ebef0c608f18f38b3c41c5f486478ad
Ladva Dharmesh <ladvadharmesh03@gmail.com>
2022-05-02 11:53:53 +0530

    .

--- src/containers/Coiuntdown.js
+++ src/containers/Coiuntdown.js
@@ -3,17 +3,7 @@ import { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 
 const Timer = () => {
-    const [seconds, setSeconds] = useState(30);
-    useEffect(() => {
-        let myInterval = setInterval(() => {
-            if (seconds > 0) {
-                setSeconds(seconds - 1);
-            }
-        }, 1000)
-        return () => {
-            clearInterval(myInterval);
-        };
-    });
+
 
     return (
         <View style={{ marginTop: 45 }}>
